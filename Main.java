75466ad1bcb556534bb9de47833cf07a4f31cd08
laybson <laybson.cunha@ccc.ufcg.edu.br>
2018-02-05 23:46:03 -0300

    Aguardando compilação

--- Main.java
+++ Main.java
@@ -1,24 +1,55 @@
+import java.io.File;
+import java.io.Reader;
+import java_cup.parser;
 import java.io.FileReader;
 import java.nio.file.Paths;
 
-public class Main {
+import java_cup.runtime.Symbol;
+// import syntaxtree.*;
+// import visitor.*;
+import java.io.*;
+import java.util.*;
 
-    public static void main(String[] args) {
+import java_cup.parser;
 
-        String rootPath = Paths.get("").toAbsolutePath().toString();
-        String subPath = "/src/";
+// public class Main {
+//
+//     public static void main(String[] args) {
+//
+//         String rootPath = Paths.get("").toAbsolutePath().toString();
+//         String subPath = "/";
+//
+//         String sourcecode = rootPath + subPath + "program.c";
+//
+//
+//         try {
+//             Parser p = new Parser(new LexicalAnalyzer(new FileReader(sourcecode)));
+//             Object result = p.parse().value;
+//
+//             System.out.println("Compilacao concluida com sucesso...");
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
 
-        String sourcecode = rootPath + subPath + "program.c";
 
-
-        try {
-            Parser p = new Parser(new Lexer(new FileReader(sourcecode)));
-            Object result = p.parse().value;
-
-            System.out.println("Compilacao concluida com sucesso...");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-}
+  public class Main {
+    	public static void main(String[] args) 	{
+      		if(args.length != 1) {
+        			System.out.println("ERROR: Invalid number of command line arguments.");
+        			System.out.println("USAGE: java Main file.txt");
+        			System.exit(1);
+      		}
+      		try {
+        			Symbol sy;
+        			Program collect;
+        			Parser parser = new Parser(new LexicalAnalyzer(new FileInputStream(args[0])));
+        			sy = parser.parse();
+      		}	catch (IOException e)	{
+      			   System.err.println("ERROR: Unable to open file: " + args[0]);
+      		}	catch (Exception e) {
+      			   e.printStackTrace(System.err);
+      		}
+    	}
+  }
