86dfe894e01f2c29bea5476e82e6f303381d7656
laybson <laybson.cunha@ccc.ufcg.edu.br>
2018-03-05 04:06:57 -0300

    Adding Types

--- examples/SemanticTest.c
+++ examples/SemanticTest.c
@@ -1,14 +1,25 @@
-int total = 0;
-int sum (int, int);
-int main () /* Main function */
-{
-printf ("This is a C basic program \n");
-total = sum (1, 1);
-printf ("Sum of two numbers : %d \n", total);
-return 0;
-}
+//int total = 0;
+//int sum (int, int);
+//int main () /* Main function */
+//{
+//printf ("This is a C basic program \n");
+//total = sum (1, 1);
+//printf ("Sum of two numbers : %d \n", total);
+//return 0;
+//}
+//
+//int sum (int a, int b)
+//{
+//return a + b;
+//}
 
-int sum (int a, int b)
+int main(void)
 {
-return a + b;
+  int contador; //variável de controle do loop
+  for(contador = 1; contador <= 10; contador++)
+  {
+      printf("%d ", contador);
+  }
+  getch();
+  return(0);
 }
